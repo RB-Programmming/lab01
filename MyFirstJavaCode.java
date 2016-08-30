0c7956cb65c2f7fae8dc600e3fdd405cbf1693b2
RYAN BARRETT <ryanbarrett@adelphi.internal>
2016-08-30 11:45:44 -0400

    The print command has been implemented.

---
+++ MyFirstJavaCode.java
@@ -0,0 +1,6 @@
+public class MyFirstJavaCode {
+   public static void main(String[] args) {
+      // Prints whatever you want in the terminal window.
+      System.out.println("The print command is functioning correctly.");
+   }
+}
